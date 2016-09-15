dc3dc9507409d21710f1deea7b334782e612021e
nysertxs <ponceluisangel96@gmail.com>
2016-09-15 09:17:26 -0500

    create clock component

---
+++ app/tests/Clock.test.jsx
@@ -0,0 +1,43 @@
+var React = require("react");
+var ReactDOM = require("react-dom");
+var expect = require("expect");
+var $ = require("jQuery");
+var TestUtils = require("react-addons-test-utils");
+
+var Clock = require("Clock");
+
+describe("Clock", () => {
+  it("should exist", () => {
+    expect(Clock).toExist();
+  });
+
+  describe("render", () => {
+    it("should render minutes", () => {
+      var clock = TestUtils.renderIntoDocument(<Clock totalSeconds={62}/>)
+      var $el = $(ReactDOM.findDOMNode(clock));
+      var timerText = $el.find(".clock-text").text();
+
+      expect(timerText).toBe("01:02");
+    });
+  });
+
+  describe("formatSeconds", () => {
+    it("should format seconds", () => {
+      var clock = TestUtils.renderIntoDocument(<Clock/>)
+      var seconds = 615;
+      var expected = "10:15";
+      var actual = clock.formatSeconds(seconds);
+
+      expect(actual).toBe(expected);
+    });
+
+    it("should format min/seconds when less than 10", () => {
+      var clock = TestUtils.renderIntoDocument(<Clock/>)
+      var seconds = 61;
+      var expected = "01:01";
+      var actual = clock.formatSeconds(seconds);
+
+      expect(actual).toBe(expected);
+    });
+  });
+});
